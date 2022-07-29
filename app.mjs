6eb78df4020fee64ed3cfc1baee0ed10241799ec
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2022-07-29 00:58:17 +0000

    refactor: Improved redaction regex, added error message and form validation.

--- app.mjs
+++ app.mjs
@@ -4,33 +4,34 @@ function startApp() {
   // so create and use/call additional functions from here
 
   // pls remove the below and make some magic in here!
-  console.log('You mean this magic!!!!');
+  let submitButton = document.getElementById("submitButton");
+  let mainText = document.getElementById("mainText");
+  let wordsToRedact = document.getElementById("wordsToRedact");
+  let errorText = document.getElementById("errorText");
+  let finalText = document.getElementById("finalText");
+  var errorWords = [];
 
-  let text = document.getElementById("input").value
+  submitButton.addEventListener("click", redactr);
 
-redactr_btn.addeventlistener(click, redactr);
-
-let mainText = document.getElementById("mainText");
-let wordsToRedact = document.getElementById("redactr-words");
-let finalText = document.getElementById("final-text");  
-let btn = document.getElementById("redactr-btn");
-
-function redactr () {
-    var newstring = mainText.value;
+  function redactr() {
+    var newString = mainText.value;
+    let wordArray = wordsToRedact.value.split(/\s+/i);
+    const errorWords = [];
     
-    let myArray= wordsToRedact.value.split(/\s+/);
-    console.log(myArray);
-    myArray.forEach (relaceWord);
-    function replaceWord (value){
-        console.log (value);
-        newString = newString.replace(value, "*".repeat(value.length));
-};
-
-finalText.innerHTML = newString;
-
-  console.log (finalText.innerHTML);
-  
-}
+    for (let i = 0; i < wordArray.length; i++) {
+      const re = new RegExp("\\s+" + wordArray[i] + "(?!\\S)", "g");
+      if (!re.test(newString)) {
+        errorWords.push(" "+wordArray[i]);
+        console.log(errorWords);
+      } else {
+        newString = newString.replace(re, "*".repeat(wordArray[i].length));
+      }
+    };
+    if (errorWords.length > 0) {
+      errorText.innerHTML = "The following whole words were not found:<br>" + errorWords.toString();
+    }
+    finalText.innerHTML = newString;
+  };
 
   const header = document.querySelector('h2');
   header.style.color = "green";
