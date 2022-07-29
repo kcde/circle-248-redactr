0f3a6fc7e5dbad33ea7aa69ed257a71c19b3da1e
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2022-07-29 14:02:25 +0000

    feature: Added statistics for time taken and word count

--- app.mjs
+++ app.mjs
@@ -9,11 +9,14 @@ function startApp() {
   let wordsToRedact = document.getElementById("wordsToRedact");
   let errorText = document.getElementById("errorText");
   let finalText = document.getElementById("finalText");
+  let timeTaken = document.getElementById("timeTaken");
+  let textCount = document.getElementById("textCount");
   var errorWords = [];
 
   submitButton.addEventListener("click", redactr);
 
   function redactr() {
+    var startTime = Date.now()
     var newString = " "+mainText.value;
     let wordArray = wordsToRedact.value.split(/\s+/i);
     const errorWords = [];
@@ -22,15 +25,20 @@ function startApp() {
       const re = new RegExp("\\s+" + wordArray[i] + "(?!\\S)", "g");
       if (!re.test(newString)) {
         errorWords.push(" "+wordArray[i]);
-        console.log(errorWords);
       } else {
         newString = newString.replace(re, " "+"*".repeat(wordArray[i].length));
       }
     };
     if (errorWords.length > 0) {
       errorText.innerHTML = "The following whole words were not found:<br>" + errorWords.toString();
+    } else {
+      errorText.innerHTML = "";        
     }
     finalText.innerHTML = newString;
+    var endTime = Date.now();
+    var elapsedTime = (endTime - startTime) + "&nbsp;milliseconds";
+    timeTaken.innerHTML = "Time taken:  "+elapsedTime;
+    textCount.innerHTML = "Amount of words in original text:  " + mainText.value.split(/\s+/).length;
   };
 
   const header = document.querySelector('h2');
